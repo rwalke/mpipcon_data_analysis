c661c8a5df00b9f067b14f8aeaf3f3c9778f88eb
Rainer Walke <walke@demogr.mpg.de>
2022-03-24 17:52:29 +0100

    add power differences

--- R/example1.R
+++ R/example1.R
@@ -36,6 +36,10 @@ power3 <- fread(file.path(data_dir,"power_consumption_MPIDR_2020_2021.csv"),
 # power is energy per unit of time 
 power3[,power1:=energy1 / 0.25]
 
+# compute the power difference
+power3[, dpower1:= c(0,diff(power1))]
+
+
 # use the time zone CET (and DST)
 power3[, date2 := dmy_hm(date1, tz="CET")]
 # double check for daylight saving time, see e.g. 2020-03-29
@@ -52,11 +56,31 @@ power3[, wday3 := wday(date3, label=TRUE)]
 check1 <- ymd_hms("2020-03-28 22:00:00", tz = "Etc/GMT-1")
 power3[check1 < date3 & date3 < check1 +dhours(6)]
 
-
+# show the power consumption histogram for 12 o'clock
 h1 <- ggplot(power3[hour3==12 & minute3==0], aes(power1)) + geom_histogram(binwidth =  1, col="yellow") +
   scale_x_continuous("power consumption at 12 am (kW)")
 h1
 
+# check power consumption jumps
+power3[,range(dpower1)]
+
+h2 <- ggplot(power3, aes(dpower1)) + geom_histogram(binwidth =  0.2, col="yellow") +
+  scale_x_continuous("power consumption jumps (kW)", limits=c(-8,8))
+h2
+
+h3 <- ggplot(power3, aes(dpower1)) + geom_histogram(binwidth =  0.2, col="yellow") +
+  scale_x_continuous("power consumption jumps (kW)", limits=c(-30,-8))
+h3
+
+h4 <- ggplot(power3, aes(dpower1)) + geom_histogram(binwidth =  0.2, col="yellow") +
+  scale_x_continuous("power consumption jumps (kW)", limits=c(8,30))
+h4
+
+# display the most significant power jumps
+power3[15 < dpower1 | dpower1 < -15,]
+
+
+# display the midday power consumption grouped by days of the week
 s1 <- ggplot(power3[hour3==12 & minute3==0], aes(x=date3, y=power1, color=factor(wday3))) + geom_point() +
   scale_x_datetime("date (GMT-1)") +
   scale_y_continuous("power consumption at 12 am (kW)", limits=c(0,60)) +
