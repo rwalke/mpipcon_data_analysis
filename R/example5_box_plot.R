a3d5ecec8ac4ac31fbb2206e183b8426a4e801c2
Rainer Walke <walke@demogr.mpg.de>
2023-04-13 10:57:42 +0200

    start adding data for 2022

--- R/example5_box_plot.R
+++ R/example5_box_plot.R
@@ -6,7 +6,7 @@
 # compute weekly and monthly box plots
 # do some illustrations
 # 
-# Rainer Walke, MPIDR Rostock, 2022
+# Rainer Walke, MPIDR Rostock, 2023
 # 
 require(data.table)
 require(lubridate)
@@ -18,7 +18,7 @@ require(Cairo)
 require(ggplot2)
 
 power2 <- get_file_by_name(
-  filename = "power_consumption_MPIDR_2020_2021.csv",
+  filename = "power_consumption_MPIDR_2020_2022.csv",
   dataset = "doi:10.17617/3.DHIBFN",
   server = "edmond.mpdl.mpg.de",
   original = TRUE
@@ -28,10 +28,10 @@ power2 <- get_file_by_name(
 data_dir <- file.path("..","..", "data")
 if (!dir.exists(data_dir)) {dir.create(data_dir)}
 
-writeBin(power2, file.path(data_dir,"power_consumption_MPIDR_2020_2021.csv"))
+writeBin(power2, file.path(data_dir,"power_consumption_MPIDR_2020_2022.csv"))
 
 # read the time series
-power3 <- fread(file.path(data_dir,"power_consumption_MPIDR_2020_2021.csv"),
+power3 <- fread(file.path(data_dir,"power_consumption_MPIDR_2020_2022.csv"),
                 encoding = "UTF-8", sep=";", dec=",", header=FALSE, skip=4, col.names=c("date1","energy1"))
 # data shows energy consumption per 15 minutes (0.25 hours)
 # power is energy per unit of time 
@@ -59,15 +59,15 @@ power3[check1 < date3 & date3 < check1 +dhours(6)]
 
 # create some boxplots
 
-p1MonthFacet <- ggplot(power3[year3<2022], aes(x=factor(month3), y=power1)) + geom_boxplot(fill="lightblue",outlier.size = 1.0) + facet_wrap(~year3) +
+p1MonthFacet <- ggplot(power3[year3<2023], aes(x=factor(month3), y=power1)) + geom_boxplot(fill="lightblue",outlier.size = 1.0) + facet_wrap(~year3) +
   scale_x_discrete("month") + scale_y_continuous("power consumption in kW")
 p1MonthFacet
 
-p1MonthFill <- ggplot(power3[year3<2022], aes(x=factor(month3), y=power1, fill=factor(year3))) + geom_boxplot(outlier.size = 1.0) +
+p1MonthFill <- ggplot(power3[year3<2023], aes(x=factor(month3), y=power1, fill=factor(year3))) + geom_boxplot(outlier.size = 1.0) +
   scale_x_discrete("month") + scale_y_continuous("power consumption in kW") + scale_fill_discrete("year")
 p1MonthFill
 
-p1WeekFill <- ggplot(power3[year3<2022], aes(x=factor(week3), y=power1, fill=factor(year3))) + geom_boxplot(outlier.size = 1.0) +
+p1WeekFill <- ggplot(power3[year3<2023], aes(x=factor(week3), y=power1, fill=factor(year3))) + geom_boxplot(outlier.size = 1.0) +
   scale_x_discrete("week", breaks=seq(1,52,2)) + scale_y_continuous("power consumption in kW") + scale_fill_discrete("year")
 p1WeekFill
 
@@ -75,14 +75,14 @@ p1WeekFill
 figures_dir <- file.path("..", "figures")
 if (!dir.exists(figures_dir)) {dir.create(figures_dir)}
 
-CairoPNG(file.path(figures_dir, "example5_boxplotMonth1.png"), width = 480, height = 360)
+CairoPNG(file.path(figures_dir, "example5_boxplotMonth1.png"), width = 720, height = 360)
 p1MonthFacet
 dev.off()
 
-CairoPNG(file.path(figures_dir, "example5_boxplotMonth2.png"), width = 480, height = 360)
+CairoPNG(file.path(figures_dir, "example5_boxplotMonth2.png"), width = 720, height = 360)
 p1MonthFill
 dev.off()
 
-CairoPNG(file.path(figures_dir, "example5_boxplotWeek.png"), width = 720, height = 360)
+CairoPNG(file.path(figures_dir, "example5_boxplotWeek.png"), width = 900, height = 360)
 p1WeekFill
 dev.off()
